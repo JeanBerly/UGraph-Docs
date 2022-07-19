21866a8ff7baf0100f3daa0959c9a8bc16165005
Jean Berly <jeanberlyy@gmail.com>
2022-07-19 11:19:03 -0300

    feat: create imgContainer for content

--- scripts/navbarObserver.js
+++ scripts/navbarObserver.js
@@ -1,8 +1,11 @@
 function setItemActive(item){
-    let oldActiveItem = document.getElementsByClassName("itemSelected")[0];
+    let oldActiveItem = document.getElementsByClassName("itemSelected");
     let newActiveItem;
     item.parentNode.childNodes.forEach(elem => {if (elem.tagName === "UL") newActiveItem = elem});
     if (oldActiveItem == null){ newActiveItem.classList.add("itemSelected"); return;}
+    oldActiveItem = [...oldActiveItem];
+    oldActiveItem.forEach((i) =>{
+        i.classList.remove("itemSelected");
+    })
     newActiveItem.classList.add("itemSelected");
-    oldActiveItem.classList.remove("itemSelected");
 }
